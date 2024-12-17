2b8f0122590222f1fd2909eaf29a46b95e9b12d0
Helleborg <sndr.hell@gmail.com>
2024-12-17 17:46:35 +0100

    ...

--- FlexibleParsingLanguage.Test/Parsing.Test.cs
+++ FlexibleParsingLanguage.Test/Parsing.Test.cs
@@ -14,12 +14,16 @@ public class Parsing
     {
         new object[] { "ConvertXmlTest", "<a><b1>bbb</b1><b2>bb2</b2></a>", "|xml.a.b2:h1", "{'h1':'bb2'}", null, null },
         new object[] { "ConvertJsonTest", "{'a':{'b1':'bbb','b2':'bb2'}}", "|json.a.b2:h1", "{'h1':'bb2'}", null, null },
-        new object[] { "ConvertConfigTest", "[{'id': 'bob'}, {'id': 'trj'}]", "|json*id@", "['bob','trj']", new List<(string, string)> { ("bob", "n53"), ("trj", "a81") }, new List<List<(string, string)>> { } },
+        new object[] { "Simple Lookup Test", "[{'id': 'bob'}, {'id': 'trj'}]", "|json*@id", "['bob','trj']", new List<(string, string)> { ("bob", "n53"), ("trj", "a81") }, new List<List<(string, string)>> { } },
 
-        new object[] { "Simple Lookup Test", "[{'id': 'id_1'}, {'id': 'id_2'}]", "|json*id@", "['id_1','id_2']", new List<(string, string)> {
-            ("id_1", "a1"),
-            ("id_2", "b2"),
-        }, new List<List<(string, string)>> {}},
+        new object[] {
+            "Lookup Test", "[{'id': 'id_1'}, {'id': 'id_2'}]", "@#id|json*@id@#name", "['na','nb']",
+            new List<(string, string)> {},
+            new List<List<(string, string)>> {
+                new List<(string, string)> { ("id", "id_1"), ("name", "na") },
+                new List<(string, string)> { ("id", "id_2"), ("name", "nb") },
+            }
+        },
 
         new object[] { "LookupTestB", "[{'name': 'name_a'}, {'name': 'name_b'}]", "@#name_config|json*@name@#id_config", "['n53','a81']", new List<(string, string)> { }, new List<List<(string, string)>> { 
             new List<(string, string)> { ("name_config", "name_a"), ("id_config", "id_a") },
--- FlexibleParsingLanguage/Compiler/Lexicalizer.Lookup.cs
+++ FlexibleParsingLanguage/Compiler/Lexicalizer.Lookup.cs
@@ -11,28 +11,15 @@ internal partial class Lexicalizer
 {
     private IEnumerable<ParseOperation> ProcessLookupOperation(ParserConfig config, ParseData parser, ParseContext ctx, AccessorData acc)
     {
-
-
-        /*
         var next = ctx.NextAccessor;
         ctx.Index++;
-        var active = ctx.ActiveId;
 
         foreach (var o in ProcessOperation(config, parser, ctx, next))
             yield return o;
-        */
-
 
         yield return new ParseOperation(ParseOperationType.LookupRead);
 
-
         if (ctx.LastWriteOp)
             yield return new ParseOperation(ParseOperationType.LookupReadValue);
-
-
-        //ctx.ActiveId = active;
-
-
-
     }
 }
